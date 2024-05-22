70f10c02234f57a1d0d43318b4753be6dec88717
Fateh OULD KRADDA <fateh.ouldkradda@bazaarvoice.com>
2024-05-22 14:27:51 +0100

    [Update] remove the apiKey test

--- src/server/RequestHandler.js
+++ src/server/RequestHandler.js
@@ -74,72 +74,72 @@ const jsonSchema = {
 
 /**
  * Creates a request handler for the given express app.
- * 
+ *
  * @param {*} app express app
- * @param {*} apiAuthenticationKey api key for authentication 
+ * @param {*} apiAuthenticationKey api key for authentication
  * @param {*} mailHandler  mail handler
  */
 const RequestHandler = (app, apiAuthenticationKey, mailHandler) => {
 
   const { validate } = new Validator();
 
-  // Using application/json parser for parsing the request body with a slightly 
+  // Using application/json parser for parsing the request body with a slightly
   // increased limit for the request body size thus allowing larger mails.
   app.use(bodyParser.json({ limit: '5mb' }));
 
   app.post(
     '/v3/mail/send',
-    validate({ body: jsonSchema.v3MailSend }), 
+    validate({ body: jsonSchema.v3MailSend }),
     (req, res) => {
 
-      const reqApiKey = req.headers.authorization;
-        
-      if (reqApiKey === `Bearer ${apiAuthenticationKey}`) {
-          
+      // const reqApiKey = req.headers.authorization;
+
+      // if (reqApiKey === `Bearer ${apiAuthenticationKey}`) {
+
         mailHandler.addMail(req.body);
-      
+
         res.status(202).header({
           'X-Message-ID': crypto.randomUUID(),
         }).send();
-      } else {
-        res.status(403).send({
-          errors: [{
-            message: 'Failed authentication',
-            field: 'authorization',
-            help: 'check used api-key for authentication',
-          }],
-          id: 'forbidden',
-        });
-      }
+      // } else {
+      //   res.status(403).send({
+      //     errors: [{
+      //       message: 'Failed authentication',
+      //       field: 'authorization',
+      //       help: 'check used api-key for authentication',
+      //     }],
+      //     id: 'forbidden',
+      //   });
+      // }
     }
   );
-      
+
   app.get('/api/mails', (req, res) => {
-    
+
     const filterCriteria = {
       to: req.query.to,
       subject: req.query.subject,
       dateTimeSince: req.query.dateTimeSince,
     };
-      
+
     const paginationCriteria = {
       page: req.query.page,
       pageSize: req.query.pageSize,
     };
-      
+
     const mails = mailHandler.getMails(filterCriteria, paginationCriteria);
-        
+
     res.send(mails);
   });
-    
+
   app.delete('/api/mails', (req, res) => {
-    
+
     const filterCriteria = {
       to: req.query.to,
     };
-      
+
     mailHandler.clear(filterCriteria);
-      
+
     res.sendStatus(202);
   });
 
@@ -150,8 +150,8 @@ const RequestHandler = (app, apiAuthenticationKey, mailHandler) => {
       const responseBody = {
         errors: error.validationErrors.body.map(validationError => {
           return {
-            field: validationError.params.missingProperty 
-              ? validationError.params.missingProperty 
+            field: validationError.params.missingProperty
+              ? validationError.params.missingProperty
               : validationError.propertyName,
             message: validationError.message,
             path: validationError.schemaPath,
